fc0224b3dfcb4eac64b0f8b65a4fe0f1049376d0
sabriye0027 <56897720+sabriye0027@users.noreply.github.com>
2019-10-28 19:22:08 +0200

    Create fork.cpp

---
+++ fork.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <conio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+void  ChildProcess(void);                          
+
+void  main(void)
+{
+	pid_t  pid;
+	int girilen_sayi;
+    int adim_sayisi=0;
+    printf (" Bir sayi giriniz : " );
+    scanf("%d",&girilen_sayi);
+    pid = fork();
+    if (pid == 0){
+        if(girilen_sayi > 0){
+		  while (girilen_sayi != 1) {
+                    if (girilen_sayi % 2 == 0)
+                    {
+                        girilen_sayi /= 2;
+                        adim_sayisi++;
+                    }
+                    else 
+                    {
+                        if (girilen_sayi % 2 == 1 && girilen_sayi!= 1)
+                        {
+                           girilen_sayi = girilen_sayi * 3 + 1;
+                            adim_sayisi++;
+                        } 
+                    }
+            } 
+		    printf("sonuc: %d\n", girilen_sayi);
+    		printf("adým sayýsý: %d", adim_sayisi );
+    	    printf(" Child process bitti. \n");
+	   }
+	
+	   else{
+		  printf("sayi pozitif degil. \n");
+	   }   
+   }
+   
+   
+    else  {
+          wait(NULL);
+          printf("Parent process \n");
+     }
+     
+
+     getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
